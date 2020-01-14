3de0fc05568a5e48d6e0f0f83c1777445b5270f1
yezhentou <925887221@qq.com>
2020-01-14 16:27:23 +0800

    投票demo

--- src/main/java/com/love/controller/SharingController.java
+++ src/main/java/com/love/controller/SharingController.java
@@ -5,6 +5,7 @@ import com.github.pagehelper.PageInfo;
 import com.love.pojo.Accepter;
 import com.love.pojo.Sharer;
 import com.love.service.SharerService;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
